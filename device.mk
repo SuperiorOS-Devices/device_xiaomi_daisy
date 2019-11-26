47b2b8d56b3df1fd1c2d1e8db58d88a72055d782
Arian <arian.kulmer@web.de>
2019-11-25 22:25:42 -0800

    daisy: Enable v1.3 clearkey drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420
    Signed-off-by: Arian <arian.kulmer@web.de>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- device.mk
+++ device.mk
@@ -216,7 +216,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Spectrum
 PRODUCT_PACKAGES += \

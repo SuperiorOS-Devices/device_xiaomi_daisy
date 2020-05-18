ced9389acee0c44a29aaa207ab69f7d067f5ed6a
TogoFire <italomellopereira@gmail.com>
2020-05-18 00:11:07 -0300

    daisy: gps: Optimize GPS
    Sources:
    gps.conf Files (Credits to BlackICE Team): https://app.box.com/s/w57s1v1n3hie7l5lk28i
    GPSTest: https://play.google.com/store/apps/details?id=com.chartcross.gpstest
    The Pool of AGPS servers: http://www.pool.ntp.org/
    
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- prop.mk
+++ prop.mk
@@ -156,7 +156,8 @@ ro.frp.pst=/dev/block/bootdevice/by-name/config
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.gps.qc_nlp_in_use=1 \
 persist.loc.nlp_name=com.qualcomm.location \
-ro.gps.agps_provider=1
+ro.gps.agps_provider=1 \
+ro.ril.def.agps.mode=1
 
 # LMKD
 PRODUCT_PROPERTY_OVERRIDES += \

c98488baf83b524bbd52e5984137d371ec27a9af
TogoFire <italomellopereira@gmail.com>
2020-07-27 19:46:05 -0300

    daisy: Add Extra Vendor

--- xtended_daisy.mk
+++ xtended_daisy.mk
@@ -18,6 +18,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some VendorExtra stuff.
+$(call inherit-product-if-exists, vendor/ExclusivePack/config.mk)
+$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
+
 # Inherit from daisy device
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 

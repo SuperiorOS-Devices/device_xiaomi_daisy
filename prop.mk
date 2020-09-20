23a9a93a95ba9be01cb054dc00340178498a52f3
Lunarixus <Nathan@lunarixus.party>
2020-09-20 02:30:12 +0000

    daisy: Add required GPU props
    These tell the ROM to use the correct Adreno drivers which fixes graphical artifacts.
    
    Credit to Sultan for the triple buffers prop.
    
    Credit to kdrag0n for noticing vulkan prop was missing.
    
    Change-Id: I3481092f3aea2be2091c8c64ca04c426a244a696
    
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- prop.mk
+++ prop.mk
@@ -254,11 +254,17 @@ persist.vendor.vt.supported=1 \
 persist.vendor.sys.cnd.iwlan=1 \
 persist.vendor.cne.logging.qxdm=3974
 
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.hardware.egl=adreno \
+ro.hardware.vulkan=adreno
+
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=4000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=8000000 \
+		ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096
 

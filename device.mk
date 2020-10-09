b2b5fd623f8d13bfe80bdee942387b2d19274cc7
TogoFire <italomellopereira@gmail.com>
2020-10-09 00:33:23 -0300

    daisy: Build keymaster 4.0 and add it to hidl entry
    -log:
    I hwservicemanager: getTransport: Cannot find entry android.hardware.keymaster@4.0::IKeymasterDevice/default in either framework or device manifest.
    
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- device.mk
+++ device.mk
@@ -222,6 +222,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey \
     android.hardware.drm@1.3-service.widevine
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
+
 # Spectrum
 PRODUCT_PACKAGES += \
    init.spectrum.rc \

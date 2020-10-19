537944c5c42f12b0abba2d3ceadb826c027ca9a0
Ali Hasan <ahb7671@gmail.com>
2020-10-19 12:15:27 +0500

    daisy: update dalvik heap values
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- prop.mk
+++ prop.mk
@@ -300,12 +300,12 @@ ro.com.google.clientidbase.ms=android-lge-rev1
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.heapstartsize=16m \
-dalvik.vm.heapgrowthlimit=256m \
+dalvik.vm.heapstartsize=8m \
+dalvik.vm.heapgrowthlimit=192m \
 dalvik.vm.heapsize=512m \
-dalvik.vm.heaptargetutilization=0.75 \
-dalvik.vm.heapminfree=4m \
-dalvik.vm.heapmaxfree=8m
+dalvik.vm.heaptargetutilization=0.6 \
+dalvik.vm.heapminfree=8m \
+dalvik.vm.heapmaxfree=16m
 
 # Charger
 PRODUCT_PROPERTY_OVERRIDES += \

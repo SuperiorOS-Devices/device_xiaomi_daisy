ed7daa12f376c78975e9bcd5d79a031fe8b81abf
pawelik001 <pawelik001@gmail.com>
2021-01-19 20:02:00 +0100

    aaaaa nerxt temp asdojnacwienrao8iwrunoiawesrnf

--- device.mk
+++ device.mk
@@ -211,7 +211,9 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libdisplayconfig \
+	libdisplayconfig.qti \
     liboverlay \
+	libqdMetaData.system \
     libqdMetaData \
     libgenlock \
     libtinyxml \
